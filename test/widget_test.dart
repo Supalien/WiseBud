5f53569ff2d955528623da937e5ba313f4c758b2
Doron <37987045+Supalien@users.noreply.github.com>
2025-09-25 01:10:52 +0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:wisebud/main.dart';
 // TODO: test
 // Issue URL: https://github.com/Supalien/WiseBud/issues/1
 
-void main() {
+void main() { // TODO: test2
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
